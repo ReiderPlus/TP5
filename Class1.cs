05aebf250165556ae49d046af3cdc47a283c4712
LEGION <LEGION@DESKTOP-CATEJDA>
2024-04-02 16:56:25 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TechProgram5
+{
+    public partial class CustomMessageBox : Window
+    {
+        public CustomMessageBox()
+        {
+            InitializeComponent();
+        }
+
+        private void RetryButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TechProgram5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string TB1 = textBox1.Text;
+            if (uint.TryParse(TB1, out uint x))
+            {
+                double rez = Math.Pow(x, 2);
+                textBox2.Text = rez.ToString();
+                CustomMessageBox customMessageBox = new CustomMessageBox();
+                bool? result = customMessageBox.ShowDialog();
+
+                if (result.HasValue && result.Value) 
+                {
+                    rez = Math.Pow(rez, 2);
+                    textBox2.Text = rez.ToString();
+                    
+                }
+                else 
+                {
+                    Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введены некорректные данные", "Тип данных", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
